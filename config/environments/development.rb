ca23b2a5fa4d26a709add71a056964bbabab85b5
Charity Fuller <charryef@gmail.com>
2018-09-29 09:09:36 -0700

    User Story 01 User Sign Up: completed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'localhost 3000'}
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDMAIL_USERNAME'],
