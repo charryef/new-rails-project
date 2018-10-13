63982e48ab11e1d1d394fd7295653c9a86d79f36
Charity Fuller <charryef@gmail.com>
2018-10-12 23:37:10 -0700

    pending completion

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,8 @@
-Rails.configuration.stripe = {
-  :publishable_key => Rails.application.credentials['PUBLISHABLE_KEY'],
-  :secret_key      => Rails.application.credentials['SECRET_KEY']
-}
+# Store the environment variables on the Rails.configuration object
+ Rails.configuration.stripe = {
+   publishable_key: Rails.application.credentials[:PUBLISHABLE_KEY],
+   secret_key: Rails.application.credentials[:SECRET_KEY]
+ }
 
-Stripe.api_key = Rails.application.credentials[:secret_key]
+ # Set our app-stored secret key with Stripe
+ Stripe.api_key = Rails.application.credentials[:SECRET_KEY]
