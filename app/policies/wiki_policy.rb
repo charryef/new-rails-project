c72ca0b5040f088df3b51234b69fa017c6d006d6
Charity Fuller <charryef@gmail.com>
2018-10-27 15:56:51 -0700

    Fixed the index view for the collaborator

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -33,16 +33,16 @@ class WikiPolicy < ApplicationPolicy
          wikis = scope.all # if the user is an admin, show them all the wikis
        elsif user.role == 'premium'
          all_wikis = scope.all
-         all_wikis.each do |wiki|
-           if !wiki.private || user == wiki.user || wiki.collaborators.include?(user)
-             wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
-           end
-         end
+          all_wikis.each do |wiki|
+            if !wiki.private || user == wiki.user || wiki.collaborators.exists?(user_id: user.id)
+              wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
+            end
+          end
        else # this is the lowly standard user
          all_wikis = scope.all
          wikis = []
          all_wikis.each do |wiki|
-           if !wiki.private || wiki.collaborators.include?(user)
+           if !wiki.private || wiki.collaborators.exists?(user_id: user.id)
              wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
            end
          end
